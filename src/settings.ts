b827dc4e9ea44fa38fff6fb26bf2e4d0509eeded
maikdevries <dev.maik.devries@outlook.com>
2024-05-16 17:34:45 +0200

    feat: define plugin settings

---
+++ src/settings.ts
@@ -0,0 +1,11 @@
+export const PLATFORM_NAME = 'homebridge-torch-platform';
+
+export const PLUGIN_NAME = 'homebridge-torch';
+
+export const MANUFACTURER = 'Maik de Vries';
+
+export const MODEL = PLUGIN_NAME;
+
+export const SERIAL_NUMBER = '404';
+
+export const FIRMWARE_REVISION = '0.0.1';
